f02d1f7021e99ea970289d0bdc11b2e992e270d1
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-11-10 20:33:39 -0500

    adding exercise solution

---
+++ exercise/solved/index.js
@@ -0,0 +1,5 @@
+$("hr").delay(500).fadeIn("slow");
+$(".time-one").delay(2000).fadeIn("slow").fadeOut("slow");
+$(".time-two").delay(5000).fadeIn("slow").fadeOut("slow");
+$(".time-three").delay(8000).fadeIn("slow").fadeOut("slow");
+$("hr").delay(9000).fadeOut("slow");
