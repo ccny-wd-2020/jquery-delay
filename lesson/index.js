56721e1b6b388d74a70da565968dd418bbe7ba1e
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-11-10 19:26:02 -0500

    adding lesson and unsolved exercise

---
+++ lesson/index.js
@@ -0,0 +1,6 @@
+$("#span-one").delay(1000).fadeIn().animate({fontSize: "30px"});
+$("#span-two").delay(2000).fadeIn().animate({fontSize: "30px"});
+$("#span-three").delay(3000).fadeIn().animate({fontSize: "30px"});
+$("#span-four").delay(4000).fadeIn().animate({fontSize: "30px"});
+
+$("#img-one").delay(6000).fadeIn().animate({height: "300px", width: "300px"});
